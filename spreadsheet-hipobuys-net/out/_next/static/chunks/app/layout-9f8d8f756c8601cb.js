4f7f6b1c2138667f56976b380be410a1a691e70a
hehaize88-code <hehaize88@gmail.com>
2026-08-25 21:24:12 +0800

    Add spreadsheet-hipobuys-net Pages out fallback
    Include the static export at both supported Pages output locations so either current dashboard path serves the site.

---
+++ spreadsheet-hipobuys-net/out/_next/static/chunks/app/layout-9f8d8f756c8601cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{8052:()=>{},8588:(e,s,n)=>{Promise.resolve().then(n.bind(n,2255)),Promise.resolve().then(n.t.bind(n,8052,23))}},e=>{e.O(0,[5,255,246,986,358],()=>e(e.s=8588)),_N_E=e.O()}]);
