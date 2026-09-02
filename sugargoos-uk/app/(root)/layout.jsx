795178093d2e73674ac5a2b382ec7a067992dbe6
hehaize88-code <hehaize88@gmail.com>
2026-09-02 15:14:21 +0800

    Move sugargoos.uk analytics into static HTML

--- sugargoos-uk/app/(root)/layout.jsx
+++ sugargoos-uk/app/(root)/layout.jsx
@@ -9,5 +9,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
+  return <html lang="en"><head><script async src="https://www.googletagmanager.com/gtag/js?id=G-K4JW68ZWVN" /><script dangerouslySetInnerHTML={{ __html: 'window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag("js",new Date());gtag("config","G-K4JW68ZWVN");' }} /></head><body>{children}</body></html>;
 }
--- sugargoos-uk/app/[lang]/layout.jsx
+++ sugargoos-uk/app/[lang]/layout.jsx
@@ -2,5 +2,5 @@ import "../globals.css";
 
 export default async function LanguageLayout({ children, params }) {
   const { lang } = await params;
-  return <html lang={lang}><body>{children}</body></html>;
+  return <html lang={lang}><head><script async src="https://www.googletagmanager.com/gtag/js?id=G-K4JW68ZWVN" /><script dangerouslySetInnerHTML={{ __html: 'window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag("js",new Date());gtag("config","G-K4JW68ZWVN");' }} /></head><body>{children}</body></html>;
 }
